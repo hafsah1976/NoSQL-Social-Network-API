283d448d217444116e46191726b3d4c4330fa279
Khan-HB <hafsah999@gmail.com>
2023-09-22 20:22:48 -0400

    moved the reactionSchema into the Thought model and deleted the Reaction.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const {reactionSchema} = require("./Reaction");
+
 
 // Thought Schema
 const thoughtSchema = new mongoose.Schema({
@@ -32,12 +32,40 @@ thoughtSchema.virtual('formattedCreatedAt').get(function () {
   return this.createdAt.toISOString(); // Customize the formatting as needed
 });
 
-// Create a virtual field "reactionCount" to calculate the length of the "reactions" array
+
+// Reaction Schema (for nested documents in reactions array)
+const reactionSchema = new mongoose.Schema({
+  reactionId: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: () => new mongoose.Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Creating a virtual field "reactionCount" to calculate the length of the "reactions" array
 thoughtSchema.virtual('reactionCount').get(function () {
-    return this.reactions.length;
-  });
+  return this.reactions.length;
+});
+
+// Defining a getter method for "createdAt" to format the timestamp on query
+reactionSchema.virtual('formattedCreatedAt').get(function () {
+  return this.createdAt.toISOString(); 
+});
 
 const Thought = mongoose.model('Thought', thoughtSchema);
+const Reaction = mongoose.model('Reaction', reactionSchema);
  
-module.exports = Thought;
+module.exports = { Thought, Reaction };
 
